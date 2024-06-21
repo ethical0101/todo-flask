529f1319ea72da9cf53c43cdace14a52e33a1432
MAD_DRAX <k.d.n8125u@gmail.com>
2024-06-21 20:59:53 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Todo.db"
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self) -> str:
+        return f"{self.Sno}--{self.title}"
+
+@app.route('/', methods=['GET','POST'])
+def druv():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']     
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+        
+    allTodo = Todo.query.all()
+    return render_template('index.html',allTodo = allTodo)
+
+
+@app.route('/details')
+def get():
+    todoall = Todo.query.all()
+    print(todoall)
+    return 'tis is bots page'
+
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/update/<int:sno>', methods=['GET','POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']     
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+        
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+     
+     
+if __name__ == '__main__':
+    app.run(debug=True, port=1357)
